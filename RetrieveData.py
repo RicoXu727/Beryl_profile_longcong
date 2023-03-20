9e4c28d568afdfc6e93064ea5636037655b7ebbf
徐珑璁 <lx2305@columbia.edu>
2023-03-20 15:03:32 -0400

    delete API key Information for safe

--- RetrieveData.py
+++ RetrieveData.py
@@ -8,10 +8,8 @@ import time
 # https://www.google.com/search?q=clean+twitter+api+data+of+profile+into+relevant+data+table&rlz=1C5MACD_enUS1021US1023&ei=nFIOZN6cFYaq5NoPpIetuAY&ved=0ahUKEwje2qjguNf9AhUGFVkFHaRDC2cQ4dUDCBA&uact=5&oq=clean+twitter+api+data+of+profile+into+relevant+data+table&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCAAQogQyBQgAEKIEMgUIABCiBDIHCAAQHhCiBDIFCAAQogQ6BAgAEEdKBAhBGABQ7gVYjDNgqEdoAHABeAKAAdIDiAGKF5IBCTQuMS4yLjMuMpgBAKABAcgBCMABAQ&sclient=gws-wiz-serp#fpstate=ive&vld=cid:9f109127,vid:1gQ6uG5Ujiw
 
 # Twitter API credentials
-consumer_key = 'CiEoUCdhGHUHBJhSswR41wB3N'
-consumer_secret = 'vwa9jCOPAptT27vCM2inqvQ1MkZRTwWvZbctzFarXBemeRQslK'
-access_token = '1357785878819659780-jFC0pjxMTCZEpEXM7hoBBXBDo5uGsM'
-access_token_secret = 'A6BKFyezJXsZnzMO6VwEBaHDqewzsAZfDMiAVGI8ayQyL'
+
+# To be on the safe side, I erased some specific information from my API.
 
 # Authenticate with the Twitter API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
